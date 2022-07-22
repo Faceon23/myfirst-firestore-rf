29fedf97905b874e09c0baea4b147aff6ec3038f
Roderick Fayson <roderickfayson@Rodericks-MacBook-Air.local>
2022-07-22 15:23:25 -0400

    more code

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-import { initializeApp, cert } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
-import { credentials } from './credentials.js'
+import { initializeApp, cert } from "firebase-admin/app"
+import { getFirestore } from "firebase-admin/firestore"
+import { credentials } from "./credentials.js"
 
 initializeApp({
-    credential: cert(credentials)
+  credential: cert(credentials),
 })
 
 const db = getFirestore()
 
-const car ={
-    make: 'Honda', model: 'Accord', year: 2017, color: 'white'
-}
-db.collection('cars').add(car)
+// db.collection('cars')
+//   .add({ make: "Mercedes", model: "EQS", year: 2022, color: "black" })
+//   .then((doc) => {
+//     console.log("Doc added:", doc.id)
+//   })
+//   .catch((err) => console.error(err))
+
+// db.collection('cars').doc('lambo')
+// .set({make: 'Lamborghini', model: 'Diablo', year: 2021, color: 'brown'})
+
+
+// db.collection('cars').doc('lambo')
+// .update({model: 'Diablo', color: 'hot pink'})
+
+// db.collection('cars').doc('lambo').get()
+// .then(doc =>{
+//     console.log(doc.id)
+//     console.log(doc.data())
+// })
+// .catch(console.error)
+
+// Get a whole collection:
+db.collection('cars').get()
+    .then(collection => {
+        collection.docs.forEach(doc => console.log(doc.id, doc.data()))
+    })
+    .catch(console.error)
+
+    // Query docs from collection:
+    db.collection('cars')
+    .where('year','>=', 2015)
+    .get()
+        .then(collection => {
+            const cars = collection.docs.map(doc => {
+                let car = doc.data()
+                car.id = doc.id
+                return car
+            })
+            
+            console.log(cars)
+        })
+        .catch(console.error)
